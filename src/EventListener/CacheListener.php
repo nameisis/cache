4c35c5dc0ab3d43affeb8cdf931ccabbf06b4ba0
k0d3r1s <k0d3r1s@gmail.com>
2019-09-03 23:01:13 +0300

    move adapters to separate repository

--- src/EventListener/CacheListener.php
+++ src/EventListener/CacheListener.php
@@ -7,7 +7,7 @@ use JsonSerializable;
 use Nameisis\Cache\Annotation\Cache;
 use Nameisis\Cache\DependencyInjection\NameisisCacheExtension;
 use Nameisis\Cache\NameisisCache;
-use Nameisis\Cache\Adapter\CacheAdapter;
+use Nameisis\Adapter\Cache\CacheAdapter;
 use Psr\Cache\CacheItemPoolInterface;
 use Psr\Cache\InvalidArgumentException;
 use ReflectionClass;
